2a2f34dc40df1a337e84ee1b040f63f5ca00ff2e
Admin <Admin@SALMAAMRO>
2025-08-09 10:15:47 +0300

    Add project files.

---
+++ Ass1AdvancedCsharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ass1AdvancedCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
