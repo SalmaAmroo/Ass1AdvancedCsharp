566536f4fdc23a3603bf0e5f70cc2642abb7943c
Admin <Admin@SALMAAMRO>
2025-08-09 10:39:54 +0300

    Q2

--- Ass1AdvancedCsharp/Program.cs
+++ Ass1AdvancedCsharp/Program.cs
@@ -5,7 +5,7 @@
     //{
     //    public static void BubbleSort<T>(T[] Arr) where T : IComparable<T>
     //    {
-           
+
 
     //        for (int i = 0; i < Arr.Length - 1; i++)
     //        {
@@ -28,6 +28,39 @@
     //                break;
     //            }
     //        }
+    //    }
+    //}
+
+
+    #endregion
+
+    #region Q2
+    //public class Range<T> where T : IComparable<T>
+    //{
+    //    public T Min { get; set; }
+    //    public T Max { get; set; }
+
+    //    public Range(T min, T max)
+    //    {
+    //        if (min.CompareTo(max) > 0)
+    //        {
+    //            T temp = min;
+    //            min = max;
+    //            max = temp;
+    //        }
+
+    //        Min = min;
+    //        Max = max;
+    //    }
+
+    //    public bool IsInRange(T value)
+    //    {
+    //        return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+    //    }
+
+    //    public double Length()
+    //    {
+    //        return Convert.ToDouble(Max) - Convert.ToDouble(Min);
     //    }
     //}
 
@@ -63,6 +96,17 @@
             //{
             //    Console.Write(name + " ");
             //}
+            #endregion
+
+            #region UsingQ2
+
+            //Range<int> R1 = new Range<int>(10, 20);
+            //Console.WriteLine("Is 15 in range? " + R1.IsInRange(15)); 
+            //Console.WriteLine("Length: " + R1.Length());
+
+            //Range<double> R2 = new Range<double>(5.5, 10.8);
+            //Console.WriteLine("Is 9.2 in range? " + R2.IsInRange(9.2)); 
+            //Console.WriteLine("Length: " + R2.Length());
             #endregion
         }
     }
