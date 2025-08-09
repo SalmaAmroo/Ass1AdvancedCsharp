e9b01f44da776174a1e35d2fc6be243345aba251
Admin <Admin@SALMAAMRO>
2025-08-09 10:25:15 +0300

    Q1

--- Ass1AdvancedCsharp/Program.cs
+++ Ass1AdvancedCsharp/Program.cs
@@ -1,10 +1,70 @@
 ﻿namespace Ass1AdvancedCsharp
 {
-    internal class Program
+    #region Q1 
+    //public class BubbleSorter
+    //{
+    //    public static void BubbleSort<T>(T[] Arr) where T : IComparable<T>
+    //    {
+           
+
+    //        for (int i = 0; i < Arr.Length - 1; i++)
+    //        {
+    //            bool swapped = false;
+
+    //            for (int j = 0; j < Arr.Length - i - 1; j++)
+    //            {
+    //                if (Arr[j].CompareTo(Arr[j + 1]) > 0)
+    //                {
+    //                    T temp = Arr[j];
+    //                    Arr[j] = Arr[j + 1];
+    //                    Arr[j + 1] = temp;
+
+    //                    swapped = true;
+    //                }
+    //            }
+
+    //            if (!swapped)
+    //            {
+    //                break;
+    //            }
+    //        }
+    //    }
+    //}
+
+
+    #endregion
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region UsingQ1
+            //int[] numbers = { 5, 1, 4, 2, 8 };
+            //Console.WriteLine("Before Swapping");
+            //foreach(var number in numbers)
+            //{
+            //    Console.Write(number + " ");
+            //}
+            //BubbleSorter.BubbleSort(numbers);
+            //Console.WriteLine("\nAfter Swapping");
+            //foreach (var number in numbers)
+            //{
+            //    Console.Write(number + " ");
+            //}
+            //Console.WriteLine("\n=================================");
+            //string[] names = { "Salma", "Ahmed", "Karim", "Ziad" };
+            //Console.WriteLine("\nBefore Swapping");
+            //foreach (var name in names)
+            //{
+            //    Console.Write(name + " ");
+            //}
+            //BubbleSorter.BubbleSort(names);
+            //Console.WriteLine("\nAfter Swapping");
+            //foreach (var name in names)
+            //{
+            //    Console.Write(name + " ");
+            //}
+            #endregion
         }
     }
 }
+
